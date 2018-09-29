98a6bad53af86e08bb57e81fe2d5a4a8282bcaa3
dschmigelski <dbschmigelski@gmail.com>
2018-09-29 01:05:30 -0500

    attempt add onInit DB

--- index.py
+++ index.py
@@ -1,10 +1,18 @@
 import json
 import datetime
-
+import boto3
 
 def handler(event, context):
+	dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('HelloWorldTable')
+    response = table.get_item(
+	    Key={
+	        'sample': 'a'
+	    }
+	)
+	item = response['Item']
     data = {
-        'output': 'Hello Fam'
+        'output': item
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
